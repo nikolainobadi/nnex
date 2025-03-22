3662c32d49831c4008e17caf35d1a1e07b8f0305
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-03-21 17:00:45 -0700

    added more CreateTap tests

--- Tests/nnexTests/CreateTapTests/CreateTapTests.swift
+++ Tests/nnexTests/CreateTapTests/CreateTapTests.swift
@@ -36,7 +36,7 @@ extension CreateTapTests {
         #expect(subfolders.isEmpty)
     }
     
-    @Test("Creates new tap folder with 'homebrew-' prefix when provided name does not include the prefix")
+    @Test("Creates new tap folder with 'homebrew-' prefix when name from arg does not include the prefix")
     func createsTapFolder() throws {
         let name = "myNewTap"
         let tapName = name.homebrewTapName
@@ -50,6 +50,31 @@ extension CreateTapTests {
         let tapFolder = try #require(try updatedTapListFolder.subfolder(named: tapName))
         
         #expect(tapFolder.name == tapName)
+    }
+    
+    @Test("Creates new tap folder with 'homebrew-' prefix when name from input does not include the prefix")
+    func createsTapFolderWithNameInput() throws {
+        let name = "myNewTap"
+        let tapName = name.homebrewTapName
+        let remoteURL = "remoteURL"
+        let gitHandler = MockGitHandler(remoteURL: remoteURL)
+        let factory = MockContextFactory(tapListFolderPath: tapListFolder.path, inputResponses: [name], gitHandler: gitHandler)
+        
+        try runCommand(factory)
+        
+        let updatedTapListFolder = try Folder(path: tapListFolder.path)
+        let tapFolder = try #require(try updatedTapListFolder.subfolder(named: tapName))
+        
+        #expect(tapFolder.name == tapName)
+    }
+    
+    @Test("Throws error when name from input is empty")
+    func createsTapInvalidNameError() throws {
+        let factory = MockContextFactory(tapListFolderPath: tapListFolder.path, inputResponses: [""])
+        
+        #expect(throws: NnexError.invalidTapName) {
+            try runCommand(factory)
+        }
     }
     
     // TODO: - need to verify other Tap properties
