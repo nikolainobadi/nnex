52c4362a4151803f5cdca6a6af3fd9a745237b9b
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-26 14:13:49 -0700

    Add new CreateTapExecutionManager

---
+++ Sources/nnex/Domain/Execution/CreateTapExecutionManager.swift
@@ -0,0 +1,127 @@
+//
+//  CreateTapExecutionManager.swift
+//  nnex
+//
+//  Created by Nikolai Nobadi on 8/26/25.
+//
+
+import Files
+import NnexKit
+import NnShellKit
+import GitShellKit
+import Foundation
+
+struct CreateTapExecutionManager {
+    private let shell: any Shell
+    private let picker: NnexPicker
+    private let gitHandler: GitHandler
+    private let context: NnexContext
+    
+    init(shell: any Shell, picker: NnexPicker, gitHandler: GitHandler, context: NnexContext) {
+        self.shell = shell
+        self.picker = picker
+        self.gitHandler = gitHandler
+        self.context = context
+    }
+}
+
+
+// MARK: - Action
+extension CreateTapExecutionManager {
+    func executeCreateTap(name: String?, details: String?, visibility: RepoVisibility) throws {
+        try gitHandler.checkForGitHubCLI()
+        
+        let tapName = try getTapName(name: name)
+        let tapListFolder = try getTapListFolder()
+        let homebrewTapName = tapName.homebrewTapName
+        let tapFolder = try tapListFolder.createSubfolder(named: homebrewTapName)
+        
+        print("Created folder for new tap named \(tapName) at \(tapFolder.path)")
+        
+        let remotePath = try createNewRepository(
+            tapName: homebrewTapName,
+            path: tapFolder.path,
+            projectDetails: details,
+            visibility: visibility
+        )
+        
+        let newTap = SwiftDataTap(name: tapName, localPath: tapFolder.path, remotePath: remotePath)
+        try context.saveNewTap(newTap)
+    }
+}
+
+
+// MARK: - Private Methods
+private extension CreateTapExecutionManager {
+    /// Retrieves the name of the new tap, prompting the user if not provided.
+    /// - Parameter name: The optional tap name.
+    /// - Returns: The resolved tap name as a string.
+    /// - Throws: An error if the name is invalid.
+    func getTapName(name: String?) throws -> String {
+        if let name, !name.isEmpty {
+            return name
+        }
+
+        let name = try picker.getRequiredInput(prompt: "Enter the name of your new Homebrew Tap.")
+
+        if name.isEmpty {
+            throw NnexError.invalidTapName
+        }
+
+        return name
+    }
+
+    /// Creates a new GitHub repository for the tap.
+    /// - Parameters:
+    ///   - tapName: The name of the tap.
+    ///   - path: The local path to initialize the repository.
+    ///   - projectDetails: Optional project details.
+    ///   - visibility: The visibility of the repository.
+    /// - Returns: The remote URL of the created repository.
+    /// - Throws: An error if the repository creation fails.
+    func createNewRepository(tapName: String, path: String, projectDetails: String?, visibility: RepoVisibility) throws -> String {
+        let projectDetails = try projectDetails ?? picker.getRequiredInput(prompt: "Enter the details for this new tap")
+        try gitHandler.gitInit(path: path)
+        print("Initialized local git repository for \(tapName)")
+
+        let remotePath = try gitHandler.remoteRepoInit(
+            tapName: tapName,
+            path: path,
+            projectDetails: projectDetails,
+            visibility: visibility
+        )
+
+        print("Created new GitHub repository for \(tapName) at \(remotePath)")
+        return remotePath
+    }
+
+    /// Retrieves the folder where new Homebrew taps will be created.
+    /// - Returns: A Folder instance for storing taps.
+    /// - Throws: An error if the folder cannot be accessed or created.
+    func getTapListFolder() throws -> Folder {
+        if let path = context.loadTapListFolderPath() {
+            return try Folder(path: path)
+        }
+
+        let homeFolder = Folder.home
+        let addNewPath = "SET CUSTOM PATH"
+        let defaultTapFolderName = "NnexHomebrewTaps"
+        let defaultPath = homeFolder.path + defaultTapFolderName
+        let prompt = "Missing Taplist folder path. Where would you like new Homebrew Taps to be created?"
+        let selection = try picker.requiredSingleSelection(title: prompt, items: [addNewPath, defaultPath])
+
+        var tapListFolder: Folder
+        if selection == addNewPath {
+            let newPath = try picker.getRequiredInput(prompt: "Enter the path where your Homebrew Taps should be created.")
+            tapListFolder = try Folder(path: newPath)
+        } else {
+            tapListFolder = try homeFolder.createSubfolder(named: defaultTapFolderName)
+        }
+
+        print("Created Homebrew Taplist folder at \(tapListFolder.path)")
+        context.saveTapListFolderPath(path: tapListFolder.path)
+        print("Saved path for Taplist folder")
+
+        return tapListFolder
+    }
+}
