ec3a4eeb95202a21e9a831aca1d5c30eceb99dcb
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-26 10:30:33 -0700

    Move ArchivedBinary to its own file

--- Sources/NnexKit/Utilities/BinaryArchiver.swift
+++ Sources/NnexKit/Utilities/BinaryArchiver.swift
@@ -8,18 +8,6 @@
 import Foundation
 import NnShellKit
 
-public struct ArchivedBinary {
-    public let originalPath: String
-    public let archivePath: String
-    public let sha256: String
-    
-    public init(originalPath: String, archivePath: String, sha256: String) {
-        self.originalPath = originalPath
-        self.archivePath = archivePath
-        self.sha256 = sha256
-    }
-}
-
 public struct BinaryArchiver {
     private let shell: any Shell
     
