21bb3e4627b79ce3b53f435d003298f0bcf98a32
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-08-25 17:45:26 -0700

    Update version to v0.8.10

--- Sources/nnex/Commands/Main/nnex.swift
+++ Sources/nnex/Commands/Main/nnex.swift
@@ -9,7 +9,7 @@ import ArgumentParser
 struct Nnex: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Utility to manage swift command line tools and streamline distrubution with Homebrew.",
-        version: "v0.8.9",
+        version: "v0.8.10",
         subcommands: [
             Brew.self,
             Build.self,
